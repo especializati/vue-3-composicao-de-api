c75b9785a49d1e63f712f32e13ce7284e66bb586
Carlos Ferreira <conta@especializati.com.br>
2021-12-09 16:13:12 -0300

    Vue 3 Setup

--- src/views/Todos/Todos.vue
+++ src/views/Todos/Todos.vue
@@ -1,3 +1,34 @@
 <template>
     <h1>Lista de Tarefas</h1>
+
+    <ul>
+        <li v-for="todo in todos" :key="todo.identify">
+            {{ todo.name }}
+        </li>
+    </ul>
+    <input type="text" v-model="name">
 </template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+    name: 'Todos',
+    setup() {
+        const todos = [
+            {identify: 1, name: 'tarefa 01', completed: true},
+            {identify: 2, name: 'tarefa 02', completed: false},
+            {identify: 3, name: 'tarefa 03', completed: true},
+        ]
+
+        const name = ref('default value')
+
+        name.value = 'teste 2'
+
+        return {
+            todos,
+            name
+        }
+    }
+}
+</script>

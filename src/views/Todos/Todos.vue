93a14e844a01642552b78927f54b6a6f29f765ad
Carlos Ferreira <conta@especializati.com.br>
2021-12-09 16:43:56 -0300

    Preloader

--- src/views/Todos/Todos.vue
+++ src/views/Todos/Todos.vue
@@ -1,12 +1,15 @@
 <template>
     <h1>Lista de Tarefas</h1>
 
+    <div v-show="loading">
+        Carregando as tarefas
+    </div>
+
     <ul>
         <li v-for="todo in todos" :key="todo.identify">
             {{ todo.title }}
         </li>
     </ul>
-    <input type="text" v-model="name">
 </template>
 
 <script>
@@ -19,13 +22,19 @@ export default {
     setup() {
         const todos = ref([])
 
+        const loading = ref(false)
+
         onMounted(() => {
+            loading.value = true
+
             TodoService.getAll()
                     .then(response => todos.value = response.data.data)
                     .catch(error => console.log(error))
+                    .finally(() => loading.value = false)
         })
 
         return {
+            loading,
             todos,
         }
     }

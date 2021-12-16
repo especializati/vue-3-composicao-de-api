b6e0e1b4889dbc7212cf7729e8500f1ebdc5fcd8
Carlos Ferreira <conta@especializati.com.br>
2021-12-16 15:01:39 -0300

    Add Todo

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Todos from '@/views/Todos/Todos.vue'
+import AddTodo from '@/views/Todos/AddTodo.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: Home
-  },
-  {
+  }, {
     path: '/todos',
     name: 'todos.index',
     component: Todos,
+  }, {
+    path: '/todos/create',
+    name: 'todos.create',
+    component: AddTodo,
   }
 ]
 
